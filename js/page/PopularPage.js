a5b3213c228a9291810bfb91f72e654bb602ab90
zmybie <2402368990@qq.com>
2019-04-19 09:49:49 +0800

    finish dynamic_top_navigator_theme

--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -1,43 +1,75 @@
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-import { createMaterialTopTabNavigator } from 'react-navigation'
+import {createMaterialTopTabNavigator} from 'react-navigation'
 
 import NaivigationUtil from '../navigation/NaivigationUtil'
 
 export default class PopularPage extends Component<Props> {
-  render() {
-    const TopTab = createMaterialTopTabNavigator({
-      PopularTab1: {
-        screen: PopularTab,
-        navigationOptions:{
-          title:'Tab1'
-        }
-      },
-      PopularTab2: {
-        screen:PopularTab,
-        navigationOptions: {
-          title: 'Tab2'
-        }
-      }
-    })
-    return <TopTab />
-  }
+    constructor(props) {
+        super(props)
+        this.tabNames = ['Java','Android','Ios','React','React Native','PHP']
+    }
+
+    _genTabs() {
+        const tabs = {};
+        this.tabNames.forEach((item,index) => {
+            //初始化顶部tab的时候 向页面传递参数
+            tabs[`tab${index}`] = {
+                screen: props => <PopularTab {...props} tabLabel={item}/>,
+                navigationOptions: {
+                    title:item
+                }
+            }
+        })
+        return tabs;
+    }
+
+
+    render() {
+        const TopTab = createMaterialTopTabNavigator(this._genTabs(),{
+            tabBarOptions: {
+                tabStyle:styles.tabStyle,
+                scrollEnabled:true,
+                upperCaseLabel:false,
+                style: {
+                    backgroundColor:'#678'
+                },
+                indicatorStyle:styles.indicatorStyle,
+                labelStyle:styles.labelStyle
+            }
+        })
+        return <TopTab/>
+    }
 }
 
 class PopularTab extends Component<Props> {
 
-  render() {
-    const {label} = this.props;
-    return (
-        <View>
-          <Text>{ label }</Text>
-          <Text onPress={() => {
-            NaivigationUtil.goPage("DetailPage")
-          }}>跳转到详情页</Text>
-        </View>
-    )
-  }
+    render() {
+        const {tabLabel} = this.props;
+        return (
+            <View>
+                <Text>{tabLabel}</Text>
+                <Text onPress={() => {
+                    NaivigationUtil.goPage("DetailPage")
+                }}>跳转到详情页</Text>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    tabStyle: {
+        minWidth:50
+    },
+    indicatorStyle: {
+        height:2,
+        backgroundColor: 'white'
+    },
+    labelStyle: {
+        fontSize:13,
+        marginTop:6,
+        marginBottom:6
+    }
+})
 
