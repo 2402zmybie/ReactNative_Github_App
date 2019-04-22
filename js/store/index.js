3cd3627497ec2520f160e7faea0f7043dda0d070
zmybie <2402368990@qq.com>
2019-04-22 13:35:20 +0800

    自定义中间件

--- js/store/index.js
+++ js/store/index.js
@@ -3,8 +3,21 @@ import thunk from 'redux-thunk'
 import reducers from '../reducer'
 import { middleware } from '../navigation/AppNavigator'
 
+//自定义中间件
+const logger = store => next => action => {
+    if(typeof action === 'function') {
+        console.log('dispatching a function');
+    }else {
+        console.log('dispatch ',action)
+    }
+    const result = next(action);
+    console.log('nextState', store.getState())
+}
+
 const middlewares = [
-    middleware
+    middleware,
+    logger,
+    thunk
 ]
 
 export default createStore(reducers, applyMiddleware(...middlewares))
